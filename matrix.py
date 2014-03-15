df2187e2f38238e67109596a3a48aa52851f791c
Tian <tiansrealm@gmail.com>
2014-03-15 15:51:27 -0400

    fixed bug in edgematrix where rows did not increase from 0 when it should after things are added

--- matrix.py
+++ matrix.py
@@ -3,20 +3,17 @@
 class Matrix(object):
 	
 	def __init__(self, row, col):
-		self.maxRow = row
-		self.maxCol = col
+		self.row = row
+		self.col = col
 		self.matrix = [ [0]*col for i in range(row)]
-		self.identity()
-	def identity(self):
-		pass
 	def set(self, row , col, value):
-		if (row <= self.maxRow and col <= self.maxCol):
+		if (row <= self.row and col <= self.col):
 			self.matrix[row][col] = value
 		else:
 			print "warning: attepmt to get a out of bound value in a Matrix"
 
 	def get(self, row , col):
-		if (row <= self.maxRow and col <= self.maxCol):
+		if (row <= self.row and col <= self.col):
 			return self.matrix[row][col]
 		else:
 			print "warning: attepmt to get a out of bound value in a Matrix"
@@ -24,14 +21,14 @@ class Matrix(object):
 		'''self(matrix) is multiplied by other and result is new matrix
 			the order is :   self.matrix * other
 		'''
-		assert(other.maxRow == self.maxCol), "invalid matrix dimension for multiplication"
-		ans = Matrix(self.maxRow, other.maxCol)
+		assert(other.row == self.col), "invalid matrix dimension for multiplication"
+		ans = Matrix(self.row, other.col)
 
 		reversedOther = zip(*other.matrix) 
-		for row in range(self.maxRow): 
-			for r2 in range(other.maxCol): #maxCol of other = maxRow of reversedOther
+		for row in range(self.row): 
+			for r2 in range(other.col): #col of other = row of reversedOther
 				s = 0;
-				for col in range(self.maxCol): 
+				for col in range(self.col): 
 					s += self.matrix[row][col] * reversedOther[r2][col] 
 				ans.set(row,r2, s)
 		return ans
@@ -47,27 +44,27 @@ class EdgeMatrix(Matrix):
 	"""EdgeMatrix
 	This matrix is actually a veritcal version.
 	Three columns with infinite many rows
-	Each row is 3 coordinates x,yz of a point
+	Each row is 3 coordinates x,y,z of a point
 	Every two rows is coupled to form a line. a.k.a edge
 	Starts with zero rows. 
 	"""
 	def __init__(self):
-		super(EdgeMatrix, self).__init__(0, 3)
+		super(EdgeMatrix, self).__init__(0, 4)
 	
-	def addLine(x1,y1,z1,x2,y2,z2):
-		self.matrix.append([x1,y1,z1])
-		self.matrix.append([x2,y2,z2])
-		
-m = EdgeMatrix()
-t = Matrix(2,3)
-for i in range(2):
-	for j in range(3):
-		t.set(i,j, i * 3 + j +1)
-t2 = Matrix(3,2)
-for i in range(3):
-	for j in range(2):
-		t2.set(i,j, i * 2 + j + 7)
+	def addLine(self,x1,y1,z1,x2,y2,z2):
+		self.matrix.append([x1,y1,z1,1])
+		self.matrix.append([x2,y2,z2,1])
+		self.row += 2
 
-print t.matrix
-print t2.matrix
-print t.mult(t2).matrix
+class TransMatrix(Matrix):
+	"""
+	4 by 4 matrix used to transformation another
+	matrix(with 4 rows) through matrix multiplication 
+	"""
+	def __init__(self):
+		super(TransMatrix, self).__init__(4, 4)	
+		self.identity()
+
+	def identity(self):
+		for i in range(self.row):
+			self.set(i,i,1)		
